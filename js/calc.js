d71d2513ecbf0a9b7c9134602fc3573b3a6cff5c
Tom Endacott <tendacott@spartaglobal.com>
2018-07-03 19:43:29 +0100

    Targeted individual buttons and got response in console

---
+++ js/calc.js
@@ -0,0 +1,19 @@
+console.log("rotaluclaC MOD");
+
+// Target individual buttons.
+var numButton7 = document.getElementsByClassName("buttonNum")[0];
+var numButton8 = document.getElementsByClassName("buttonNum")[1];
+
+// Get response in console. Get input onto 'screen'.
+numButton7.addEventListener("click", function(){
+  console.log("Button was clicked");
+});
+numButton8.addEventListener("click", function(){
+  console.log("Button was clicked");
+});
+
+
+
+// Target class operator into array
+// event listener "click", handleClick, function action
+// event listener for operators "click", handleClick, function action
