d9aafa8826c14f8c2f5f2db82cbf6ab0c28428b7
Tom Endacott <tendacott@spartaglobal.com>
2018-07-03 19:52:12 +0100

    Buttons function and add their relevant integer to the screen

--- js/calc.js
+++ js/calc.js
@@ -1,19 +1,66 @@
 console.log("rotaluclaC MOD");
 
-// Target individual buttons.
+// Target individual number buttons.
 var numButton7 = document.getElementsByClassName("buttonNum")[0];
 var numButton8 = document.getElementsByClassName("buttonNum")[1];
+var numButton9 = document.getElementsByClassName("buttonNum")[2];
+var numButton4 = document.getElementsByClassName("buttonNum")[3];
+var numButton5 = document.getElementsByClassName("buttonNum")[4];
+var numButton6 = document.getElementsByClassName("buttonNum")[5];
+var numButton1 = document.getElementsByClassName("buttonNum")[6];
+var numButton2 = document.getElementsByClassName("buttonNum")[7];
+var numButton3 = document.getElementsByClassName("buttonNum")[8];
 
 // Get response in console. Get input onto 'screen'.
-numButton7.addEventListener("click", function(){
-  console.log("Button was clicked");
+numButton7.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "7";
 });
-numButton8.addEventListener("click", function(){
-  console.log("Button was clicked");
+numButton8.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "8";
 });
-
-
+numButton9.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "9";
+});
+numButton4.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "4";
+});
+numButton5.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "5";
+});
+numButton6.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "6";
+});
+numButton1.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "1";
+});
+numButton2.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "2";
+});
+numButton3.addEventListener("click", function(event){
+  document.getElementById("screen").innerHTML = "3";
+});
 
 // Target class operator into array
+
+
 // event listener "click", handleClick, function action
 // event listener for operators "click", handleClick, function action
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
